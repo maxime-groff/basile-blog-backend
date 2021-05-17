96b4fdcca9c6ddd666a0c4365221e379a80b31b5
Maxime Groff <maxime.groff@engie.com>
2021-05-17 19:43:13 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b90a146eaea78885c6e66298b9a743f6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e6c2beac-9e80-4144-a7c6-ffef0bba2f86'
+};
